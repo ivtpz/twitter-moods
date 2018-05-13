ca43069c0cee79105cbc856738d26dc162e5583d
Ivey Topaz <iveytopaz@gmail.com>
2018-05-13 13:00:02 +0100

    Update

--- scripts/publishKeywords.sh
+++ scripts/publishKeywords.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
-cp ../data/stateTweetKeywords.json ../public/src/visualizations/twitter-moods
+# cp ../data/stateTweetKeywords.json ../public/src/visualizations/twitter-moods
+curl -X POST -H "Content-Type: application/json" -d '{"filePath":"data/stateTweetKeywords.json"}' http://localhost:5555/api/images
